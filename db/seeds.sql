d5f510600dc5cf28204aef454a27b0e2cdd81829
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2019-07-28 10:55:13 -0700

    Added burger schemas and seeds into SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+        ('Cheese Burger'), ('Jalapeno Double Cheeseburge'),
+        ('Big Mac'), ('Double Wester Bacon Cheeseburger'),
+        ('Whopper'), ('Chicken Carb-O-Nera'),
+        ('Filet-o-Fish'), ('Southern Pork Belly Grilled Cheese'),
+        ('Quarter Pounder'), ('Nashville Screamin Hot Chicken Sandwich'),
+        ('Whopper'), ('Manwich'),
+        ('P.B & Jellousy')
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
+var routes = require('./controllers/burgers_controller');
+app.use('/', routes);
+
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
